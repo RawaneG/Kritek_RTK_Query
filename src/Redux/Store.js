fec4d6bfd9d175356d2fc4b4a3cddbf27e12b50c
RawaneG <rawane.meissa@gmail.com>
2023-08-24 03:11:16 +0000

    Kritek_RTK_Query_Test

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,8 +1,12 @@
-import { configureStore,combineReducers } from "@reduxjs/toolkit";
-import logger from "redux-logger";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import { Reducer } from "./Reducer";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
-const rootreducer=combineReducers({user:Reducer});
-const Store=configureStore({reducer:rootreducer,middleware:[thunk,logger]})
+const rootreducer = combineReducers({ user: Reducer });
+const Store = configureStore({
+  reducer: rootreducer,
+  middleware: [thunk, logger],
+});
+
 export default Store;
